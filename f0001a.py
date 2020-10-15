839a4e05b7bc40e7777d25e3b48daac5f06056aa
kissgergo0905 <55017111+kissgergo0905@users.noreply.github.com>
2020-10-15 08:59:26 +0200

    Add files via upload

---
+++ f0001a.py
@@ -0,0 +1,7 @@
+print('...Hogyha én ezt tudtam volna')
+print('Köpönyeget hoztam vóna!')
+
+# Lehet kombinálni, mivel az egyszerű macskaköröm (') és a hagyományos (") ugyan az mégis nem ugya az.
+# Ha én ' és " kezdem a programot akkor nem fog tudni futni. Összeomlik.
+
+
